642a1c3dd748c1104c3910d5ec2e86db64bb104c
Sabiha-Moradi <47026524+Sabiha-Moradi@users.noreply.github.com>
2025-11-09 03:21:49 +0100

    ETL: loader finished

--- data_ingestion/loaders.py
+++ data_ingestion/loaders.py
@@ -1,42 +1,83 @@
 from langchain_community.document_loaders import PyPDFLoader, UnstructuredURLLoader
-from typing import List
+from typing import List, Set
 from langchain.schema import Document
+import re
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin, urlparse
 
 CURRICULUM_PDF_PATH = "../docs/1193_17_BS_Wirtschaftsinformatik.pdf"
-CURRICULUM_URL = "https://studienhandbuch.jku.at/texte/1193_17_BS_Wirtschaftsinformatik.pdf"
-STUDIENHANDBUCH_URL = "https://studienhandbuch.jku.at/curr/1193?id=1193&lang=de"
+IDEAL_STUDY_PLAN_PDF_PATH = "../docs/idealtypischerStudienverlauf.pdf"
+DOMAIN_BASE = "https://studienhandbuch.jku.at/"
+STUDIENHANDBUCH_URL = DOMAIN_BASE + "curr/1193?id=1193&lang=de"
 
-def load_documents_from_pdf(file_path: str = CURRICULUM_PDF_PATH) -> List[Document]:
+def load_pages_from_pdf(file_path: str) -> List[Document]:
     try:
         print(f"Pfad für PDF: {file_path}")
         loader = PyPDFLoader(file_path)
-        documents = loader.load()
-        print(f"--> {len(documents)} Seiten geladen.")
-        return documents
+        pages = loader.load()
+        print(f"--> {len(pages)} Seiten geladen.")
+        return pages
     except Exception as e:
         print(f"FEHLER beim Laden der PDF: {e}")
         return []
 
-
-def load_documents_from_url(url: str = STUDIENHANDBUCH_URL) -> List[Document]:
+def get_links_from_url(url: str, content: str) -> Set[str]:
+    links = set()
+    # Basis-Link gefolgt von mindestens einer Ziffer
+    link_pattern = re.compile(r"^" + re.escape(DOMAIN_BASE) + r"\d+$")
     try:
-        print(f"Pfad für Website: {url}")
-        loader = UnstructuredURLLoader(urls=[url])
-        documents = loader.load()
-        #TODO load all links from Studienhandbuch for subject Prerequisites
-        return documents
+        soup = BeautifulSoup(content, 'html.parser')
+
+        for link_tag in soup.find_all('a', href=True):
+            href = link_tag['href']
+            absolute_url = urljoin(url, href)
+
+            if link_pattern.match(absolute_url):
+                links.add(absolute_url)
+
     except Exception as e:
-        print(f"FEHLER beim Laden der URL: {e}")
-        return []
+        print(f"Fehler während Parsen der links von {url}: {e}")
+
+    return links
+
+
+def load_sites_from_url(url: str = STUDIENHANDBUCH_URL) -> List[Document]:
+    all_webpages = []
+
+    try:
+        print(f"Hauptseite: {url}")
+        response = requests.get(url, timeout=15)
+        response.raise_for_status()
+
+        embedded_links = get_links_from_url(url, response.text)
+        print(f"--> {len(embedded_links)} eingebettete Links gefunden.")
+        #print(f"Beispiellink: {list(embedded_links)[31]}")
+
+        urls_to_load = list(embedded_links)
+        urls_to_load.append(url)
+
+        loader = UnstructuredURLLoader(urls=urls_to_load)
+        webpages = loader.load()
+
+        all_webpages.extend(webpages)
+
+    except Exception as e:
+        print(f"FEHLER beim Laden der URL(s): {e}")
+
+    print(f"--> {len(all_webpages)} Dokumente geladen.")
+    return all_webpages
 
 
 def load_all_curriculum_data() -> List[Document]:
     all_documents = []
 
-    pdf_docs = load_documents_from_pdf()
-    all_documents.extend(pdf_docs)
+    curriculum_pdf = load_pages_from_pdf(CURRICULUM_PDF_PATH)
+    all_documents.extend(curriculum_pdf)
+    study_plan_pdf = load_pages_from_pdf(IDEAL_STUDY_PLAN_PDF_PATH)
+    all_documents.extend(study_plan_pdf)
 
-    url_docs = load_documents_from_url()
+    url_docs = load_sites_from_url()
     all_documents.extend(url_docs)
 
     return all_documents
