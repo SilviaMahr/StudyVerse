c2dd3e2c84e4cf5ab49635e444bcfe4c17530f06
12219457 <k12219457@jku.students.at>
2025-11-25 16:04:44 +0100

    display username on landingpage

--- frontend/src/app/pages/landing/landing.component.ts
+++ frontend/src/app/pages/landing/landing.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {SidebarComponent} from '../../components/sidebar/sidebar.component';
 import {PreselectionComponent} from '../../components/preselection/preselection.component';
+import {ProfileService} from '../../../services/profile.service';
 
 @Component({
   selector: 'app-landing',
@@ -15,9 +16,29 @@ import {PreselectionComponent} from '../../components/preselection/preselection.
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
-  constructor() {
+  username: string | undefined;
+  isLoading: boolean = true;
+  constructor(private profileService: ProfileService) {
+
   }
 
   ngOnInit() {
+    this.loadUserProfile();
+  }
+
+  loadUserProfile() {
+    this.profileService.getMyProfile().subscribe({
+      next: (profile) => {
+        this.username = profile.username;
+        this.isLoading = false;
+        console.log(this.username)
+      },
+      error: (err) => {
+        console.error('Konnte Profil nicht laden', err);
+        this.username = '';
+        this.isLoading = false;
+        console.log("Das hat nicht funktioniert")
+      }
+    });
   }
 }
